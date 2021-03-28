a4e92c0634bb3bc6b9c0e6d741900b88668b73df
Aleksei Yarmolenko <68028444+Lekasey@users.noreply.github.com>
2021-03-29 01:04:06 +0200

    test failure. fixed again

--- src/components/TransactionList/TransactionItem.js
+++ src/components/TransactionList/TransactionItem.js
@@ -1,9 +1,9 @@
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import styles from './TransactionItem.module.css'
 
 
 const TransactionItem = ({ type, amount, currency}) => (
-    <tr className={styles.tableRow.1}>
+    <tr className={styles.tableRow}>
         <td className={styles.type}>{type}</td>
         <td className={styles.amount}>{amount}</td>
         <td className={styles.currency}>{currency}</td>
